aa7e7fa4f5af8e84d79f01ad62e53bb859272ec7
Ayan Dasgupta <ayan.dasgupta@hyland.com>
2025-07-14 07:42:56 -0400

    Ran pre-commit

--- activiti-core/activiti-bpmn-converter/src/test/java/org/activiti/bpmn/converter/DeleteMeTest.java
+++ activiti-core/activiti-bpmn-converter/src/test/java/org/activiti/bpmn/converter/DeleteMeTest.java
@@ -19,7 +19,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeleteMeTest {
-
     @Test
     public void testCoveredUTLocally() {
         DeleteMe deleteMe = new DeleteMe();
