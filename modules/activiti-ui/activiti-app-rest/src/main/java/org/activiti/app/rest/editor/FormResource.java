8201a949a23c1c35ffac31c1324a4fc7178fee64
Ali Bonaftizadeh <79264950+ali-bonaftizadeh@users.noreply.github.com>
2025-08-22 03:15:29 +0330

    Add AI form generator
    * Add LLM connector module
    
    * Fix ai service bug
    
    * Edit dsl
    
    ---------
    
    Co-authored-by: Bozorgmehr Zia <bozorgmehr1381@gmail.com>

--- modules/activiti-ui/activiti-app-rest/src/main/java/org/activiti/app/rest/editor/FormResource.java
+++ modules/activiti-ui/activiti-app-rest/src/main/java/org/activiti/app/rest/editor/FormResource.java
@@ -18,13 +18,11 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.activiti.app.model.editor.FormSaveRepresentation;
 import org.activiti.app.model.editor.form.FormRepresentation;
+import org.activiti.app.service.editor.AISupportedActivitiFormService;
 import org.activiti.app.service.editor.ActivitiFormService;
+import org.activiti.form.model.FormDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Tijs Rademakers
@@ -33,27 +31,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest/form-models")
 public class FormResource {
 
-  @Autowired
-  protected ActivitiFormService formService;
+    @Autowired
+    protected ActivitiFormService formService;
 
-  @RequestMapping(value = "/{formId}", method = RequestMethod.GET, produces = "application/json")
-  public FormRepresentation getForm(@PathVariable String formId) {
-    return formService.getForm(formId);
-  }
+    @Autowired
+    protected AISupportedActivitiFormService aiSupportedActivitiFormService;
 
-  @RequestMapping(value = "/values", method = RequestMethod.GET, produces = "application/json")
-  public List<FormRepresentation> getForms(HttpServletRequest request) {
-    String[] formIds = request.getParameterValues("formId");
-    return formService.getForms(formIds);
-  }
+    @RequestMapping(value = "/{formId}", method = RequestMethod.GET, produces = "application/json")
+    public FormRepresentation getForm(@PathVariable String formId) {
+        return formService.getForm(formId);
+    }
 
-  @RequestMapping(value = "/{formId}/history/{formHistoryId}", method = RequestMethod.GET, produces = "application/json")
-  public FormRepresentation getFormHistory(@PathVariable String formId, @PathVariable String formHistoryId) {
-    return formService.getFormHistory(formId, formHistoryId);
-  }
+    @RequestMapping(value = "/values", method = RequestMethod.GET, produces = "application/json")
+    public List<FormRepresentation> getForms(HttpServletRequest request) {
+        String[] formIds = request.getParameterValues("formId");
+        return formService.getForms(formIds);
+    }
 
-  @RequestMapping(value = "/{formId}", method = RequestMethod.PUT, produces = "application/json")
-  public FormRepresentation saveForm(@PathVariable String formId, @RequestBody FormSaveRepresentation saveRepresentation) {
-    return formService.saveForm(formId, saveRepresentation);
-  }
+    @RequestMapping(value = "/{formId}/history/{formHistoryId}", method = RequestMethod.GET, produces = "application/json")
+    public FormRepresentation getFormHistory(@PathVariable String formId, @PathVariable String formHistoryId) {
+        return formService.getFormHistory(formId, formHistoryId);
+    }
+
+    @RequestMapping(value = "/{formId}", method = RequestMethod.PUT, produces = "application/json")
+    public FormRepresentation saveForm(@PathVariable String formId, @RequestBody FormSaveRepresentation saveRepresentation) {
+        return formService.saveForm(formId, saveRepresentation);
+    }
+
+    @RequestMapping(value = "/generate-from-prompt", method = RequestMethod.GET, produces = "application/json")
+    public FormDefinition generateFormFromPrompt(@RequestParam String prompt) {
+        return aiSupportedActivitiFormService.generateFormFromPrompt(prompt);
+    }
 }
